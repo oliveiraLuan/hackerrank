ca97ccdd11374417aa0b6ba65f6f10e6d8fa80b8
Luan de Oliveira <luan012012@hotmail.com>
2024-05-28 22:01:43 -0300

    feat: Exercise03.java

---
+++ regex/src/Exercise03.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Exercise03 {
+
+    public static void main(String[] args) {
+
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\w{3}\\W\\w{10}\\W\\w{3}"); // Use \\ instead of using \ 
+
+    }
+}
+
+class Regex_Test3 {
+
+    public void checker(String Regex_Pattern){
+
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }
+
+}
