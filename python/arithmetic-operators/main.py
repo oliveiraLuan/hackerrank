45abf570fd94221d2d6ea7f3e194433149a9d2e4
Luan de Oliveira <luan012012@hotmail.com>
2024-06-07 23:36:30 -0300

    feat: arithmetic operators

---
+++ python/arithmetic-operators/main.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    total = a + b;
+    difference = a - b;
+    product = a * b;
+
+    print(total);
+    print(difference);
+    print(product);
